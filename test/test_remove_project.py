bb95b3b2912fcdb7187ccdf9c4efcbc95691ecbd
ol6a <fedorova.o.a@bk.ru>
2020-11-20 20:30:05 +0300

    Исправлена ошибка предусловия

--- test/test_remove_project.py
+++ test/test_remove_project.py
@@ -2,7 +2,7 @@ import random
 from model.project import Project
 
 def test_remove_project(app):
-    if len(app.project.get_project_list()==0):
+    if len(app.project.get_project_list())==0:
         app.project.create_project(Project(name="New Project"))
     old_projects=app.project.get_project_list()
     project=random.choice(old_projects)
